21b62d3bc62b97a01e5e693a78bee79b7eeaa1e0
SimonJalilUNSJ <simonjalilcruz@gmail.com>
2020-12-16 20:16:33 -0300

    Modificacion de test bench y rx

--- Receptor/rx.vhd
+++ Receptor/rx.vhd
@@ -3,13 +3,16 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity rx is 
+	generic (
+		clksXbit	:integer := 217
+	);
 	port (
 			clk 			:in std_logic;								--Entrada de clk.
 			reset_low 	:in std_logic;								--Entrada de reset en bajo.
 			rxd 			:in std_logic;								--Entrada del receptor.
-			clk_entry	:in std_logic_vector(1 downto 0);	--Entradas de clk.
+			--clk_entry	:in std_logic_vector(1 downto 0);	--Entradas de clk.
 			rx_byte		:out std_logic_vector(7 downto 0);	--Salida de byte recibido.
-			led			:out std_logic								--led indicador de transmision (cuando esta apagado esta transmitiendo).
+			led			:out std_logic								--led indicador de transmision (cuando esta apagado esta recibiendo).
 			);
 	end rx;
 
@@ -18,17 +21,16 @@ architecture behav of rx is
 	type FSM_states is (idle, start, data_bit, stop);
 	signal current_state, next_state :FSM_states;
 	
-	type tabla is array (0 to 3) of integer;
-	
 	attribute syn_encoding	:string;
 	attribute syn_encoding of FSM_states : type is "one hot";
 	
-	constant TABLA_DIVISORES : tabla := (5208, 2604, 650, 216);	--4800baud, 9600baud, 38400baud, 115200baud.
+	--type tabla is array (0 to 3) of integer;
+	--constant TABLA_DIVISORES : tabla := (5208, 2604, 650, 216);	--4800baud, 9600baud, 38400baud, 115200baud.
 
 	signal baud_cont 	:integer range 0 to 5209 := 0;
 	signal bit_cont	:integer range 0 to 8 := 0;
  	
-	signal rxbuff	:std_logic_vector(7 downto 0);
+	signal rxbuff	:std_logic_vector(7 downto 0) := "11111111";
 	
 begin 
 
@@ -52,7 +54,7 @@ begin
 					end if;
 				when start => 
 					--if(baud_cont >= TABLA_DIVISORES(to_integer(unsigned(clk_entry)))/2) then 
-					if(baud_cont >= 5208) then
+						if(baud_cont = (clksXbit-1)/2) then 
 						baud_cont <= 0;
 						if (rxd = '1') then
 							next_state <= idle;
@@ -65,7 +67,7 @@ begin
 					end if;
 				when data_bit =>
 					--if (baud_cont < TABLA_DIVISORES(to_integer(unsigned(clk_entry)))) then 
-					if (baud_cont < 5208) then 
+					if(baud_cont = (clksXbit-1)/2) then 
 						baud_cont <= baud_cont + 1;
 						next_state <= data_bit;
 					else 
@@ -81,7 +83,7 @@ begin
 					end if;
 				when stop => 
 					--if (baud_cont < TABLA_DIVISORES(to_integer(unsigned(clk_entry)))) then
-					if (baud_cont < 5208) then
+					if(baud_cont = (clksXbit-1)/2) then 
 						baud_cont <= baud_cont + 1;
 						next_state <= stop;
 					else 
--- Receptor/tb_rx.vhd
+++ Receptor/tb_rx.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity tb_rx is 
 end tb_rx;
@@ -11,7 +12,7 @@ architecture behav of tb_rx is
 				clk 			:in std_logic;								
 				reset_low 	:in std_logic;								
 				rxd 			:in std_logic;								
-				clk_entry	:in std_logic_vector(1 downto 0);	
+				--clk_entry	:in std_logic_vector(1 downto 0);	
 				rx_byte		:out std_logic_vector(7 downto 0);	
 				led			:out std_logic								
 				);
@@ -19,32 +20,80 @@ architecture behav of tb_rx is
 
 	signal tb_clk			:std_logic := '0';
 	signal tb_reset_low	:std_logic := '1';
-	signal tb_rxd 			:std_logic;
-	signal tb_clk_entry	:std_logic_vector(1 downto 0) := "00";
-	signal tb_rx_byte 	:std_logic_vector(7 downto 0);
+	signal tb_rxd 			:std_logic := '1';
+	--signal tb_clk_entry	:std_logic_vector(1 downto 0) := "00";
+	signal tb_rx_byte 	:std_logic_vector(7 downto 0) := "11111111";
 	signal tb_led 			:std_logic;
 	
-	constant semiperiod :time := 104166ns;		--4800baud.
-	--constant semiperiod :time := 52083ns;	--9600baud.
-	--constant semiperiod :time := 13020ns;	--38400baud.
-	--constant semiperiod :time := 4340ns;		--115200baud;
+	constant semiperiod	:time 	:= 20ns;
+	constant clksXbit 	:integer :=217;
+	constant perXbit 		:time		:=5208ns ;
 	
+--	type tabla is array (0 to 3) of integer;
+--	constant CLK_POR_BIT : tabla := (10416, 5208, 1302, 434);
+--	
+--	type periodos is array (0 to 3) of time;
+--	constant PERIODO_BIT	: periodos := (208us,104us,26us,8680ns);
+	
+	procedure byte_rx (
+		data_in				:in std_logic_vector(7 downto 0);
+		--clk_select			:in std_logic_vector(1 downto 0);
+		signal serial_out	:out std_logic 
+	) is 
+	begin
+		--Envio bit de start
+		serial_out <= '0';
+		--wait for PERIODO_BIT(to_integer(unsigned(clk_select)));	
+		wait for perXbit;
+		
+		--Envio byte de dato
+		for i in 0 to 7 loop 
+			serial_out <= data_in(i);
+			--wait for PERIODO_BIT(to_integer(unsigned(clk_select)));	
+			wait for perXbit;
+		end loop;
+		
+		--Envio bit de stop
+		serial_out <= '1';
+		--wait for PERIODO_BIT(to_integer(unsigned(clk_select)));	
+		wait for perXbit;
+	end byte_rx;	
+
 begin 
-	uut: rx port map (clk => tb_clk, reset_low => tb_reset_low, rxd => tb_rxd, clk_entry => tb_clk_entry, rx_byte => tb_rx_byte, led => tb_led);
+	--uut: rx port map (clk => tb_clk, reset_low => tb_reset_low, rxd => tb_rxd, clk_entry => tb_clk_entry, rx_byte => tb_rx_byte, led => tb_led);
+	uut: rx port map (clk => tb_clk, reset_low => tb_reset_low, rxd => tb_rxd, rx_byte => tb_rx_byte, led => tb_led);
 	
 	clk_gen: tb_clk <= not tb_clk after semiperiod;
 	
 	data_gen: process 
-		begin 
-			tb_rxd <= '1';
-			wait until tb_clk = '1';
-			tb_rxd <= '0';
-			wait until tb_clk = '1';
-			tb_rxd <= '1';
-			wait until tb_clk = '1';
-			
-			for i in 0 to 100000 loop 
-				wait until tb_clk = '1';
-			end loop; 	
-		end process;
+	begin 
+		--Envio un byte al receptor
+		wait until rising_edge(tb_clk);
+		--byte_rx(x"37",tb_clk_entry,tb_rxd);
+		byte_rx(x"37",tb_rxd);
+		wait until rising_edge(tb_clk);
+	
+		--chequeo si el envio fue correcto
+		if tb_rx_byte = x"37" then 
+			report "Testeo aprobado" severity note;
+		else
+			report "Testeo desaprobado" severity note;
+		end if;
+	
+		--assert false report "Tests Complete" severity failure;
+	end process;
 end behav;
+	
+--	data_gen: process 
+--		begin 
+--			tb_rxd <= '1';
+--			wait until tb_clk = '1';
+--			tb_rxd <= '0';
+--			wait until tb_clk = '1';
+--			tb_rxd <= '1';
+--			wait until tb_clk = '1';
+--			
+--			for i in 0 to 100000 loop 
+--				wait until tb_clk = '1';
+--			end loop; 	
+--		end process;
